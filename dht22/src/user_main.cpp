3e46fee9103b79727622156896c4d9cae25a7dc9
niklas <nikko242@gmail.com>
2016-11-12 13:37:56 +0100

    Fixar

--- dht22/src/user_main.cpp
+++ dht22/src/user_main.cpp
@@ -23,44 +23,83 @@ extern void uart_div_modify(int,int);
 
 //
 // TODO: Snygga till
-// Fel detektering
-// konvertering
-// Checksumma m.m.
+// Checksumma
+// signbit temp.
+// timing
+/*
+class BitSet16
+{
+private:	
+	uint16 _bits;
+	
+public:	
+	BitSet16()
+	{
+		_bits = 0;
+	}
+	BitSet16(uint16 bits)
+	{
+		_bits = bits;
+	}
+	void Clear()
+	{
+		_bits = 0;
+	}
+	void SetBit(int nr,bool value)
+	{
+		nr = nr % 16;
+		
+		_bits != 0x0001<<nr;
+	}
+	bool GetBit(int nr)
+	{
+		nr = nr % 16;
+		
+		return (_bits & 0x0001<<nr) > 0;
+	}
+	
+	uint16 GetValue()
+	{
+		return _bits;
+	}
+	unsigned char GetValue8()
+	{
+		return (unsigned char)(0x00ff & _bits);
+	}
+};
+*/
+typedef enum {
+	DHTOK = 0,				// Evrything fine
+	DHTERROR_BUSHANG = 1,  // No response
+	DHTERROR_TREH_INVALID = 2, // Invalid timing of read values
+	DHTERROR_DATA_INVALID = 3,
+	DHTERROR_BUFFEROVERFLOW = 4,
+	DHTERROR_CHECKSUM = 5,
+} DhtState;
 
-#define BUFFLEN 50
+#define DATABITS 40
 class Dht22
 {
 	
 private:
 	core::GPIO m_gpio;
-	int m_numIrqs;
-//	xQueueHandle m_q;
 	
 	bool m_lastFlankPositive;
 	uint32 m_positiveFlankStart;
+
+	int m_bitIdx;
+	bool m_dataBits;
+	DhtState m_state;
 	
-	
-	unsigned char m_buffer[BUFFLEN];
-	int m_buffIdx;
-	bool m_buffOverflow;
+	unsigned char m_dataBuffer[DATABITS];
 	
 public:
 	Dht22(int pinNumber):m_gpio(pinNumber)
 	{
-		m_numIrqs = 0;
-		m_buffIdx =0;
-//		m_q = NULL;
+		clearReadState();
 	}
 	bool configure()
 	{
-/*		m_q = xQueueCreate(41,sizeof(unsigned char));
-		
-		if(m_q == NULL)
-		{
-			// TODO error handling
-			return false;
-		}
-*/			
 		// Configure io-pin, we use pullup to keep high, altough an input
 		m_gpio.modeInput();
 		m_gpio.enablePullUp(true);	// Pull pin high
@@ -79,53 +118,113 @@ public:
 		// .. and release the bus, so the sensor could drive it for data
 		m_gpio.modeInput();
 		
-		// Clear queue and irq state
-		clearIrqState();
+		// clear our state
+		clearReadState();
 		
 		uint32 result = 0;
 		
-		while(true)
+		printf("Waiting");		
+		vTaskDelay(1000 / portTICK_RATE_MS);
+		
+		if(readingDone())
 		{
-			printf("Waiting");
-			vTaskDelay(5000 / portTICK_RATE_MS);
-			
-			for(int i = 0; i < m_buffIdx;i++)
+			*humidity = 0;
+			*temperature = 0;
+			int checksum = 0;
+			// Alright, then process values
+			for(int i = 0; i < DATABITS;i++)
 			{
-				printf("[%d] %d\n",i +1,m_buffer[i]);
+				bool v;		
+			    unsigned char t = m_dataBuffer[i];
+				printf("%d=>",t);	
+				// Validate & convert
+				if(t >= 50 && t <= 100) // High ?
+				{
+					// Yep. 
+					v = true;
+				}
+				else if(t >= 22 && t < 50) // Low ?
+				{
+					// Yep.
+					v = false;
+				}
+				else
+				{
+					// Crap.
+					m_state = DHTERROR_DATA_INVALID;			
+					return false;
+				}
+				printf("%d\n",v?1:0);
+				
+				if(v)
+				{
+					if(i < 16)	// 0-15 humidity
+					{
+						*humidity |= 0x8000>>i;
+					}
+					else if(i >= 16 && i < 32) // 16-31 Temp
+					{
+						*temperature |= 0x8000>>(i - 16);
+					}
+					else	// Check sum
+					{
+						checksum |= 0x80>>(i - 32);
+					}
+				}
 			}
+			
+			// Ok, lets check the check sum
+			int calculatedChecksum = (*humidity & 0xff00)>>8 + (*humidity & 0x00ff) + (*temperature & 0xff00)>>8 + (*temperature & 0x00ff);
+			printf("%d == %d\n",calculatedChecksum,checksum);
+			if(calculatedChecksum != checksum)
+			{
+				*humidity = 0;
+				*temperature = 0;
+				
+				m_state =DHTERROR_CHECKSUM;
+				return false;
+				
+			}
+			printf("\n");							
+			return true;
+
+			
 		}
-	/*	
-		
-		printf("Unqueueing");
-		for(int i = 0; i < 41;i++)
+		else
 		{
-			xQueueReceive(m_q,&result,1000);
-			printf("(%d) [%d] %d\n",m_numIrqs, i +1,result);
+			// Timeout
 		}
-*/		
-		vTaskDelay(5000 / portTICK_RATE_MS);
-		
-		*humidity = m_numIrqs;
-		
-		return true;
+		return false;
 	}
 	
+	DhtState getLastResult()
+	{
+		return m_state;
+	}
 private:
-	void clearIrqState()
+	bool readingDone()
+	{
+		return m_bitIdx == DATABITS;
+	}
+	
+	void clearReadState()
 	{
 		m_lastFlankPositive = false;
-		m_buffIdx = 0;
-		m_buffOverflow = false;
-//		xQueueReset(m_q);
+		m_positiveFlankStart = 0;
+		m_bitIdx = 0;
+		m_state = DHTOK;
+		m_dataBits = false;
 	}
 	void wakeSensor()
 	{
+		// Drive bus low
 		m_gpio.modeOutput();	
 		m_gpio.write(false);	
 		os_delay_us(20000);
-		
+//		busyWait(20000);
 		m_gpio.write(true);	// Issue "go" to sensor		
-		os_delay_us(40);	
+//		busyWait(40);
+		os_delay_us(40);
 	}
     void busyWait(uint32 micros)
 	{
@@ -135,6 +234,9 @@ private:
 	}
 	void isr()
 	{
+		if(m_state != DHTOK)
+			return;
+		
 		if(m_gpio.read())
 		{
 			m_lastFlankPositive = true;
@@ -142,28 +244,28 @@ private:
 		}
 		else
 		{
-			m_lastFlankPositive = false;
-			uint32 t = (WDEV_NOW() - m_positiveFlankStart);
+			// Is this a correct value ..?
+			if(m_lastFlankPositive)
+			{
+				// Yes, we have had a positive flank before
+				// have we recive start signal from sensor ..?
+				if(!m_dataBits)
+				{
+					// nope, then this must be Treh
+					m_dataBits = true;
+				}
+				else
+				{
+					// Data is flowing
+					if(m_bitIdx < DATABITS)
+					{
+						m_dataBuffer[m_bitIdx++] = (WDEV_NOW() - m_positiveFlankStart);
+					}
+				}
+			}
 			
-			if(m_buffIdx < BUFFLEN && t < 256)
-			{
-				m_buffer[m_buffIdx++] = t;
-			}
-			else
-			{
-				// Sorry, crap out ..
-				m_buffOverflow = true;
-			}
-			// 
-			// We borrow to calculated
-//			m_positiveFlankStart =(WDEV_NOW() - m_positiveFlankStart);
-//			portBASE_TYPE woken;
-			// We have a negative flank .. let them now
-//			portBASE_TYPE result = xQueueSendFromISR(m_q,&m_positiveFlankStart,&woken);
+			m_lastFlankPositive = false;
 		}
-		
-		
-		m_numIrqs++;
 	}
 	static void sharedIsr(void *instance)
 	{
@@ -198,11 +300,11 @@ void helloTask(void *pvParameters)
 
 	if(dht22.read(&hum,&temp))
 	{
-		printf("Success %d\n",hum);			
+		printf("Success %d %d\n",hum,temp);	
 	}
 	else
 	{
-		printf("Failed\n");			
+		printf("Failed\n");
 	}
 
 	while(1);
